8926acdd085e7bcaded212dc080074007578d21b
Breno Cabral <breno.ara.cabral@gmail.com>
2025-07-11 19:21:41 -0300

    refactor: ajust context and pointers

--- client/client.go
+++ client/client.go
@@ -14,14 +14,15 @@ type Cotacao struct {
 }
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
+	defer cancel()
 
-	var cotacao Cotacao
-	err := fetchCotacao(&cotacao)
+	cotacao, err := fetchCotacao(ctx)
 	if err != nil {
 		log.Fatalf("[ERROR] Erro ao acessar servidor: %v", err)
 	}
 
-	err = writeCotacao(&cotacao)
+	err = writeCotacao(*cotacao)
 	if err != nil {
 		log.Fatalf("[ERROR] Erro ao escrever no arquivo: %v", err)
 	}
@@ -29,38 +30,38 @@ func main() {
 	log.Printf("[Info] Cotação (Dólar: %s) escrita em cotacao.txt", cotacao.Bid)
 }
 
-func fetchCotacao(cotacao *Cotacao) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*300)
-	defer cancel()
-
+func fetchCotacao(ctx context.Context) (*Cotacao, error) {
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer res.Body.Close()
 
+	var cotacao Cotacao
 	err = json.NewDecoder(res.Body).Decode(&cotacao)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return &cotacao, nil
 }
 
-func writeCotacao(cotacao *Cotacao) error {
+func writeCotacao(cotacao Cotacao) error {
 	file, err := os.Create("cotacao.txt")
 	if err != nil {
 		return err
 	}
 	defer file.Close()
+
 	_, err = file.WriteString("Dólar: " + cotacao.Bid)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
